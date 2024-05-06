88f9f33a90651d523257c7852cbf27af31b93445
Prosper Atu <atu.prosper@gmail.com>
2024-05-06 11:39:36 +0100

    getting fixes

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,15 +1,40 @@
 #!/usr/bin/env bash
 # Sets up a web server for deployment of web_static.
-# the frontend of the application
-
-LOCATION="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
 
 sudo apt-get update
-sudo apt-get -y upgrade
-sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "Testing Nginx config" | sudo tee /data/web_static/releases/test/index.html
+sudo apt-get install -y nginx
+
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
+
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i "35i $LOCATION" /etc/nginx/sites-available/default
-sudo service nginx start
+
+sudo chown -R ubuntu /data/
+sudo chgrp -R ubuntu /data/
+
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" | sudo tee /etc/nginx/sites-available/default
+
+sudo service nginx restart
